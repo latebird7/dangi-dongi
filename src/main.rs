c1aff54aafe8674c08a6e397fcb9ee576160bfd2
nightowl <203645148+latebird7@users.noreply.github.com>
2025-12-20 19:17:49 +0100

    feat: replace vec with HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 #[derive(Clone, Debug)]
 struct User {
     name: String,
@@ -7,82 +9,80 @@ struct User {
 
 #[derive(Debug)]
 struct Users {
-    users: Vec<User>,
+    users: HashMap<String, User>,
 }
 
 impl Users {
     fn add_user(&mut self, name: String) {
-        // Check if user already exists:
-        for user in &self.users {
-            if user.name == name {
-                println!("User {} already exists.", name);
-                return;
-            }
+        if self.users.contains_key(&name) {
+            println!("User {} already exists.", name);
+            return;
         }
-        self.users.push(User {
-            name: name.clone(),
-            amount_paid: 0.0,
-            net_balance: 0.0,
-        });
+        self.users.insert(
+            name.clone(),
+            User {
+                name: name.clone(),
+                amount_paid: 0.0,
+                net_balance: 0.0,
+            },
+        );
         println!("User {} added.", name);
     }
 
     fn record_payment(&mut self, user: &str, amount: f64) {
-        let mut found = false;
-        for u in self.users.iter_mut() {
-            if u.name == user {
+        match self.users.get_mut(user) {
+            Some(u) => {
                 u.amount_paid += amount;
-                found = true;
-                println!("User {} paid {}.", user, amount);
+                println!("{} for user {} added.", amount, user);
             }
-        }
-        if !found {
-            println!("User {} not found.", user);
+            None => {
+                println!("User {} not found.", user);
+            }
         }
     }
 
     fn remove_payment(&mut self, user: &str, amount: f64) {
-        let mut found = false;
-        for u in self.users.iter_mut() {
-            if u.name == user {
+        match self.users.get_mut(user) {
+            Some(u) => {
                 u.amount_paid -= amount;
-                found = true;
                 println!("{} for user {} removed.", amount, user);
             }
-        }
-        if !found {
-            println!("User {} not found.", user);
+            None => {
+                println!("User {} not found.", user);
+            }
         }
     }
 
     fn calculate_total_payments(&mut self) -> () {
-        if self.users.len() == 0 {
-            println!("No users to calculate payments for.");
+        if self.users.len() < 2 {
+            println!("Not enough users to calculate payments.");
             return;
         }
-        let total: f64 = self.users.iter().map(|u| u.amount_paid).sum();
-        let share_per_person = total / self.users.len() as f64;
+        let total: f64 = self.users.values().map(|u| u.amount_paid).sum();
+        dbg!(total);
+        let share_per_user = total / self.users.len() as f64;
 
-        for user in &mut self.users {
-            user.net_balance = user.amount_paid - share_per_person;
+        for (_, value) in &mut self.users {
+            value.net_balance = value.amount_paid - share_per_user;
         }
-
-        println!(
-            "self.users after calculating total payments: {:#?}",
-            self.users
-        );
     }
 }
 
 fn main() {
-    let mut users = Users { users: Vec::new() };
+    let mut users = Users {
+        users: HashMap::new(),
+    };
+    users.add_user(String::from("A"));
     users.add_user(String::from("A"));
     users.add_user(String::from("B"));
     users.add_user(String::from("C"));
-    users.record_payment("A", 50.0);
+    users.record_payment("A", 60.0);
     users.record_payment("B", 30.0);
+    users.record_payment("D", 30.0);
+    users.remove_payment("B", 10.0);
+    users.remove_payment("A", 20.0);
+    users.remove_payment("D", 20.0);
     users.calculate_total_payments();
 
     println!("users {:#?}", users.users);
-
 }
