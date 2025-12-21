90ad563b02f4f6430932bc4d3b48686f16609367
nightowl <203645148+latebird7@users.noreply.github.com>
2025-12-21 16:38:04 +0100

    feat: add calculation on how to settle up

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,46 @@ impl Users {
         for (_, value) in &mut self.users {
             value.net_balance = value.amount_paid - share_per_user;
         }
+
+        {
+            let eps = 1e-6;
+            let mut creditors: Vec<(String, f64)> = self
+                .users
+                .iter()
+                .filter_map(|(name, u)| if u.net_balance > eps { Some((name.clone(), u.net_balance)) } else { None })
+                .collect();
+
+            let mut debtors: Vec<(String, f64)> = self
+                .users
+                .iter()
+                .filter_map(|(name, u)| if u.net_balance < -eps { Some((name.clone(), u.net_balance)) } else { None })
+                .collect();
+
+            creditors.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+            debtors.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+            let mut i = 0usize;
+            let mut j = 0usize;
+            while i < creditors.len() && j < debtors.len() {
+                let available = creditors[i].1;
+                let owed = -debtors[j].1;
+                let transfer = available.min(owed);
+
+                println!("{} pays {} {:.2}", debtors[j].0, creditors[i].0, transfer);
+
+                creditors[i].1 -= transfer;
+                debtors[j].1 += transfer; // debtors store negative values
+
+                if creditors[i].1 <= eps { i += 1; }
+                if debtors[j].1 >= -eps { j += 1; }
+            }
+
+            for (_, u) in &mut self.users {
+                if u.net_balance.abs() <= eps {
+                    u.net_balance = 0.0;
+                }
+            }
+        }
     }
 }
 
@@ -90,17 +130,8 @@ fn main() {
     users.add_user(String::from("C"));
     users.record_payment("A", 60.0);
     users.record_payment("B", 30.0);
-    users.record_payment("C", 30.0);
-
-    users.remove_payment("B", 60.0);
-    users.remove_payment("D", 20.0);
+    users.record_payment("C", 40.0);
     
     users.calculate_total_payments();
-    println!("users {:#?}", users.users);
 
-    users.remove_user(String::from("B"));
-    users.record_payment("B", 30.0);
-    users.calculate_total_payments();
-
-    println!("users {:#?}", users.users);
 }
